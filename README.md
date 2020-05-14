6edbe83f197097d1cf18dc42dac5f4ca8ef337fc
MingChiao Wu <q9898989@gmail.com>
2020-05-14 16:43:34 +0800

    switch resource

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 a cra project test react hook and redux-thunk
 
-refer to [redux doc](https://redux.js.org/advanced/example-reddit-api), use raddit api
+refer to [redux doc](https://redux.js.org/advanced/example-reddit-api), use reddit api
--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchPostsIfNeeded } from './actions';
+import { fetchPostsIfNeeded, selectSubreddit } from './actions';
 import './App.css';
 
 function App() {
   const dispatch = useDispatch()
   const subreddit = useSelector(state => state.selectedSubreddit)
+
   useEffect(() => {
     dispatch(fetchPostsIfNeeded(subreddit));
   }, [subreddit, dispatch])
+
   const posts = useSelector(state => {
     const { postsBySubreddit, selectedSubreddit } = state;
     return selectPosts(postsBySubreddit, selectedSubreddit);
   })
+
   return (
     <div className="App">
+      <div>
+        <button onClick={() => dispatch(selectSubreddit('reactJS'))}>reactJs</button>
+        <button onClick={() => dispatch(selectSubreddit('javascript'))}>javascript</button>
+        <button onClick={() => dispatch(selectSubreddit('webdev'))}>webdev</button>
+        <button onClick={() => dispatch(selectSubreddit('web'))}>web</button>
+      </div>
       <p>{subreddit}</p>
       {posts.length > 0 && (
         posts.map(post => (
